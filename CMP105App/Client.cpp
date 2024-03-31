54545f203a5afabb7b818028d988f0b862eecc28
Kahoneki <shinyzorua6@gmail.com>
2024-03-31 06:26:39 +0100

    Cleaned up debugs and did some bug fixes
    Still doesn't work but the clients are at least receiving packets now!

--- CMP105App/Client.cpp
+++ CMP105App/Client.cpp
@@ -11,24 +11,21 @@ Client::Client(sf::IpAddress _serverAddress, unsigned short _serverPort) {
 	sf::Packet packet;
 	packet << PacketCode::AddClient;
 	if (socket.send(packet, serverAddress, serverPort) != sf::Socket::Done) {
-		std::cerr << "Failed to connect to server." << std::endl;
+		std::cerr << "Failed to send connection request to server." << std::endl;
+	}
+	
+	//Get client index from server
+	sf::IpAddress tempIp;
+	unsigned short tempPort;
+	sf::Packet data;
+	if (socket.receive(data, tempIp, tempPort) != sf::Socket::Done) {
 		connectedToServer = false;
+		std::cerr << "Failed to connect to server." << std::endl;
 	}
 	else {
 		std::cout << "Successfully connected to server.\n";
+		data >> clientIndex;
 		connectedToServer = true;
-
-		//Get client index from server
-		sf::IpAddress tempIp;
-		unsigned short tempPort;
-		sf::Packet data;
-		if (socket.receive(data, tempIp, tempPort) != sf::Socket::Done) {
-			std::cerr << "Failed to receive client index from server." << std::endl;
-		}
-		else {
-			std::cout << "Successfully received client index from server.\n";
-			data >> clientIndex;
-		}
 	}
 
 	socket.setBlocking(false);
@@ -36,7 +33,6 @@ Client::Client(sf::IpAddress _serverAddress, unsigned short _serverPort) {
 
 
 Client::~Client() {
-	std::cout << "brr";
 	//Send message to server to tell it to remove client from vector
 	socket.setBlocking(true);
 	sf::Packet outgoingPacket;
@@ -53,7 +49,7 @@ Client::~Client() {
 sf::Socket::Status Client::SendDataToClient(int outgoingClientIndex, PacketCode code, sf::Packet incomingPacket) {
 	//Combine client index and packet code into the data packet so it can be sent to the server
 	sf::Packet outgoingPacket;
-	outgoingPacket << outgoingClientIndex << static_cast<std::underlying_type<PacketCode>::type>(code);
+	outgoingPacket << static_cast<std::underlying_type<PacketCode>::type>(code) << outgoingClientIndex;
 	outgoingPacket.append(incomingPacket.getData(), incomingPacket.getDataSize());
 	return socket.send(outgoingPacket, serverAddress, serverPort);
 }
