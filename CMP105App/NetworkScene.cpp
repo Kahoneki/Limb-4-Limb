5e6858ba5edb39040f6fa4aa961ca194771cd29d
Kahoneki <shinyzorua6@gmail.com>
2024-05-11 07:47:14 +0100

    removed unnecessary commented code

--- CMP105App/NetworkScene.cpp
+++ CMP105App/NetworkScene.cpp
@@ -160,18 +160,6 @@ void NetworkScene::update(float dt) {
 
 	}
 	HealthBarUpdate();
-
-
-	//if (matchEnd) {
-	//	std::cout << "Winner: " << (winningPlayerNMI == opponentNetworkManagerIndex) ? "Opponent. :|\n" : "Me! :]\n";
-	//	std::cout << "New ranking: " << updatedRanking << '\n';
-	//	AccountManager accountManager{ AccountManager::getInstance() };
-	//	accountManager.setValues(accountManager.getUsername(), updatedRanking);
-
-	//	//TEMP: GO BACK TO MAIN MENU
-	//	MainMenu* mainMenu{ new MainMenu(window, input, sceneManager) };
-	//	sceneManager.LoadScene(mainMenu);
-	//}
 }
 
 
